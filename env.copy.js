4d381a06dff43abdedc15088977fd25a996af9c8
Harshit Dubey <harshit@arcitech.ai>
2026-03-31 16:47:13 +0530

    Add env copy

---
+++ env.copy.js
@@ -0,0 +1,4 @@
+// PROJECT_PASS = examock@ic20
+// DATABASE_URL="postgresql://postgres:examock@ic20@db.tyjqjccwcjlvciurukqy.supabase.co:5432/postgres"
+// PORT=3000
+// CLIENT_URI="http://localhost:5173/"
